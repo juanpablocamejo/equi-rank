94294588f185de3d01f4fb041fa621508d8666e6
Marcelo Castagna <mcastagna@srt.gob.ar>
2019-09-25 17:44:08 -0300

    wip tabla y orden

--- js/rank.controller.js
+++ js/rank.controller.js
@@ -2,29 +2,43 @@
     angular.module('rank')
         .controller('rankCtrl', function () {
             vm = this;
-            vm.datos = []
+            vm.datos = [];
+            vm.tiempoReferencia = 0.000;
 
             for (var i = 0; i < 8; i++) {
                 vm.datos.push(defaultItem(i+1))
             }
+
             function defaultItem(ord) {
                 return {
                     orden: ord,
                     binomio: null,
-                    tiempo: '0000000',
-                    faltas: 0,
+                    tiempo: 0,
+                    faltas: Math.floor(Math.random() * 10),
                     comentario: null,
-                    puesto: null
+                    puesto: null,
+                    diferencia : 0
                 };
             }
+
             function comparar(a, b) {
-                va = '' + (a.faltas || 0) + (a.tiempo || 0);
-                vb = '' + (b.faltas || 0) + (b.tiempo || 0);
+                va = '' + (a.diferencia || 0) + (a.faltas || 0);
+                vb = '' + (b.diferencia || 0) + (b.faltas || 0);
                 return va - vb;
             }
 
-            vm.calcular = function () {
-                sorted = vm.datos.sort(comparar);
+            vm.actualizarDiferencias = function(){
+                vm.datos = vm.datos.map(function(item){
+                    item.diferencia = Math.abs(vm.tiempoReferencia - item.tiempo);
+
+                    return item;
+                });
+
+                vm.datos.sort(comparar);
+            }
+
+            vm.exportar = function () {
+                
             }
         })
 
