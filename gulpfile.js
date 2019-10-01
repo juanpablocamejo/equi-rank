91e51a94a37e5c0a8e1b2347c82f5ac730d3648c
Marcelo Castagna <mcastagna@srt.gob.ar>
2019-10-01 13:51:22 -0300

    manifest e iconos

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
-const { src, dest } = require('gulp')
+const { src, dest, parallel } = require('gulp')
 const htmlmin = require('gulp-htmlmin');
 const inline = require('gulp-inline')
 const uglify = require('gulp-uglify-es').default
 const cleanCSS = require('gulp-clean-css');
 
-exports.default = function () {
-    return src('index.html')        
+function copyManifest() {
+    return src('./manifest.json')
+        .pipe(dest('dist/'))
+}
+
+function copyIcons() {
+    return src('./icons/*.png')
+        .pipe(dest('dist/icons/'))
+}
+
+function bundle() {
+    return src('index.html')
         .pipe(inline({
             base: '.',
             css: cleanCSS,
@@ -18,3 +28,5 @@ exports.default = function () {
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(dest('dist/'))
 }
+
+exports.default = parallel(copyIcons, copyManifest, bundle);
