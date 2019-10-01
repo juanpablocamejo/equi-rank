00670c8740fa4e9c2a5618adf342daa5c9c0326e
Marcelo Castagna <mcastagna@srt.gob.ar>
2019-10-01 15:29:10 -0300

    cambio en el beforeunload

--- js/main.js
+++ js/main.js
@@ -8,6 +8,13 @@ initApp = () => {
             });
         });        
     }    
+
+    window.addEventListener("beforeunload", function (e) {
+        var confirmationMessage = "\o/";
+      
+        (e || window.event).returnValue = confirmationMessage;     //Gecko + IE
+        return confirmationMessage;                                //Webkit, Safari, Chrome etc.
+      });
 }
 
 initApp();
