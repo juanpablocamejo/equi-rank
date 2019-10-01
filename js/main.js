9cbfb3b0cb9209d8ecc9201b34668137d7ebb780
Marcelo Castagna <mcastagna@srt.gob.ar>
2019-10-01 15:01:44 -0300

    prueba de sacar iconos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-initApp = () => { 
-    window.addEventListener('load', function() {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js');
-        }
-    });
+initApp = () => {     
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+            navigator.serviceWorker.register('/service-worker.js').then(function(registration){
+                console.log('Service worker successfully registered on scope', registration.scope);
+            }).catch(function (error) {
+                console.log('Service worker failed to register');
+            });
+        });        
+    }    
 }
 
 initApp();
