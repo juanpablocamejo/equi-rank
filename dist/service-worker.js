81d7bb0e7689933a681c1ea52e0e1898ae47b23f
Marcelo Castagna <mcastagna@srt.gob.ar>
2019-10-01 15:24:08 -0300

    service worker andando

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,7 +1,21 @@
 var cacheName = "sgtoilet-cache";
 var filesToCache = [
-  "./",
-  "./index.html"
+  "./",  
+  "./index.html",
+  "./icons/apple-icon-57x57.png",
+  "./icons/apple-icon-60x60.png",
+  "./icons/apple-icon-72x72.png",
+  "./icons/apple-icon-76x76.png",
+  "./icons/apple-icon-114x114.png",
+  "./icons/apple-icon-120x120.png",
+  "./icons/apple-icon-144x144.png",
+  "./icons/apple-icon-152x152.png",
+  "./icons/apple-icon-180x180.png",
+  "./icons/icon-192x192.png",
+  "./icons/favicon-32x32.png",
+  "./icons/favicon-96x96.png",
+  "./icons/favicon-16x16.png",
+  "./icons/ms-icon-144x144.png"
 ];
 self.addEventListener("install", function(e) {
   e.waitUntil(
